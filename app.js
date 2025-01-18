ec09e7c7111504ed3fce5aa75970a24c01c38463
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-01-18 15:20:17 -0300

    Add new function to eval operation

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-// basic functions
+let num1 = 0;
+let num2 = 0;
+let operator = '';
 
+// basic functions
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+function operate(operation, a, b) {
+  return operation(a, b);
+}
+
+
+console.log(operate(multiply, 1, 2));
